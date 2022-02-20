7c5515299b05d1d958a35821831b79c0a646f870
Alfonso Lopez Cruz <alfonso2081lopez@gmail.com>
2022-02-20 17:06:30 -0500

    commit tree a1f4ba1320964e01c502e4449ccd962a9ecc5094

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Write your solution in this file!
+var customerName = 'bob'
+const leastFavoriteCustomer = 'sam'
+function upperCaseCustomerName(){
+    if (customerName = 'BOB'){
+        return true;
+    }
+}
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+function overwriteBestCustomer(){
+  bestCustomer = 'maybe bob'
+}
+function changeLeastFavoriteCustomer(){
+     leastFavoriteCustomer = 'cat';
+}
